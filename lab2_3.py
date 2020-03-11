abb16a410b7f52310f13d4a74e9b9c65d398ccc1
nikewalce1 <56953587+nikewalce1@users.noreply.github.com>
2020-03-11 17:53:38 +0300

    Add files via upload

---
+++ lab2_3.py
@@ -0,0 +1,34 @@
+﻿import os
+import re
+#Задан  путь  к  директории  с  музыкальными  файлами  (в  названии 
+#которых  нет  номеров,  а  только  названия  песен)  и  текстовый  файл, 
+#хранящий полный список песен с номерами и названиями в виде строк 
+#формата «01. Freefall [6:12]». Напишите скрипт, который корректирует 
+#имена файлов в директории на основе текста списка песен. 
+# path = r'D:\Programming\python\lab2_3' #os.getcwd()
+# print(path)
+# for root, dirs, files in os.walk(path):
+	# for _files in files:
+		# if _files.endswith('.com'):
+			# print(_files)
+# file = open('lab2_3/music.txt', 'r')
+# for text in file:
+	# print(text)
+Play_List = {}
+path_directory = r'E:\Python\lab2_3' #os.getcwd()
+print(path_directory)
+f= open (r'E:\Python\lab2_3\music.txt')
+lines = f.readlines()
+f.close()
+pattern_number = r'(\d+\.\s)[\w\s-]+'
+pattern_name = r'\d+\.\s([\w\s-]+)'
+for line in lines:
+    re_num = ''.join(re.findall(pattern_number, line))
+    re_name =''.join(re.findall(pattern_name, line)).rstrip()
+    Play_List[re_name] = re_num
+print(Play_List)
+print(os.listdir(path_directory))
+for name in os.listdir(path_directory):
+    if name.endswith('.mp3') and name[:-4] in Play_List :
+        os.rename(name, Play_List[name[:-4]]+name)
+        print('aueee')
---
+++ lab2_4.py
@@ -0,0 +1,21 @@
+﻿import re
+from colorama import init
+from colorama import Fore, Back, Style
+init()
+# #Nапишите  скрипт,  который  позволяет  ввести  с  клавиатуры  имя 
+# #текстового файла, найти в нем с помощью регулярных выражений все 
+# #подстроки определенного вида, в соответствии с вариантом.
+# #Вариант 1: найдите все даты – подстроки вида «11-05-2014». 
+
+
+path = open('lab2_4.txt', 'r')
+lines = path.readlines()
+pattern = r'\d\d-\d\d-\d\d\d\d+'
+count = 0
+for line in lines:
+    count+=1
+    date = re.findall(pattern, line)
+    if date:
+        for new_date in date:
+            print('Строка {0}, позиция {1} : найдено {2}'.format(count, line.index(new_date), new_date))
+
