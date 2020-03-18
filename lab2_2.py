a019be76439c81ba80abe9dc1443736f4eace1e1
nikewalce1 <56953587+nikewalce1@users.noreply.github.com>
2020-03-18 11:59:36 +0300

    Update lab2_2.py

--- lab2_2.py
+++ lab2_2.py
@@ -5,31 +5,41 @@ import hashlib
 # (MD5).  ‘айлы  могут  иметь  одинаковое  содержимое,  но  отличатьс¤ 
 # именами. —крипт должен вывести группы имен обнаруженных файлов-
 # дубликатов. 
-#certutil —hashfile "D:\Steam\Steam.exe" MD5 - определение хеша
 
-#Open file
-#print(os.startfile(r'C:\Users\Admin\Desktop\Style\1.jpg'))
-path = r'D:\Programming\python' 
+path = r'E:\Python\lab2_3' 
 #path = os.getcwd()
-sum_dict = {}
-for root, dirs, files in os.walk(path):
-    for file in files:
-        path = os.path.join(root, file)
-        with open(path, 'r') as f:
-            content = f.read().encode('utf-8').strip()
+files = os.listdir(path)
+filecount = []
+for file in files:
+    with open(path+'\\'+file, 'rb') as f:
+        content = f.read()
+        f.close()
+        filecount.append(hashlib.md5(content).hexdigest())
 
-        checksum = hashlib.md5(content).hexdigest()
-        if checksum in sum_dict:
-            sum_dict[checksum] += [path]
-        else:
-            sum_dict[checksum] = [path]
+for i in range(len(files) - 1):
+    for j in range(i + 1, len(files)):
+        if filecount[i] == filecount[j]:
+            print(files[i], ' is ', files[j])
 
-sum_dict = {sum: group for sum, group in sum_dict.items() if len(group) > 1}
-counter = 1
-for group in sum_dict.values():
-    print('Group {}:'.format(counter))
-    for path in group:
-        print(path)
+# sum_dict = {}
+# for root, dirs, files in os.walk(path):
+    # for file in files:
+        # path = os.path.join(root, file)
+        # with open(path, 'r') as f:
+            # content = f.read().encode('utf-8').strip()
 
-    counter += 1
-    print()
+        # checksum = hashlib.md5(content).hexdigest()
+        # if checksum in sum_dict:
+            # sum_dict[checksum] += [path]
+        # else:
+            # sum_dict[checksum] = [path]
+
+# sum_dict = {sum: group for sum, group in sum_dict.items() if len(group) > 1}
+# counter = 1
+# for group in sum_dict.values():
+    # print('Group {}:'.format(counter))
+    # for path in group:
+        # print(path)
+
+    # counter += 1
+    # print()
