2692a911283c7f4ae6b59a60931852c3b2510d56
nikewalce1 <56953587+nikewalce1@users.noreply.github.com>
2020-05-05 20:15:40 +0300

    Create main.py

---
+++ lab3/lab3_4/main.py
@@ -0,0 +1,7 @@
+from StringFormater import StringFormatter  
+string = "ksl;jasfj aafdsa a dfs as  123 dfasfafsa"
+print(StringFormatter.DelWords(string, 3))
+print(StringFormatter.ReplaceNum(string)) 
+print(StringFormatter.CreateSpace(string))
+print(StringFormatter.SortSize(string))
+print(StringFormatter.SortLex(string))
