76cef1869a4c01f631a00d9e8e2789dbd787dff2
nikewalce1 <56953587+nikewalce1@users.noreply.github.com>
2020-05-05 20:11:14 +0300

    Create main.py

---
+++ lab3/lab3_1/main.py
@@ -0,0 +1,8 @@
+# encoding: utf8
+from Fraction import Fraction
+frac = Fraction(7,2)
+print(-frac)      #выводит -7/2
+print(~frac)      #выводит 2/7
+print(frac**2)    #выводит 49/4
+print(float(frac))#выводит 3.5
+print(int(frac))  #выводит 3
